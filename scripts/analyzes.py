0504cde6c0ab03de1492cf1efe263a6cd14e5129
Artur Rodrigues Rocha Neto <keizerzilla@gmail.com>
2018-04-27 16:43:19 -0300

    sad boys

--- scripts/analyzes.py
+++ scripts/analyzes.py
@@ -3,13 +3,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-src = "/home/artur/pesquisa/centerprofile/nonneutral/"
-dst = "/home/artur/pesquisa/centerprofile/nonneutral_sort/"
+def analyzes(src, cloud, dst):
+	path = src + cloud
+	df = np.array(pd.read_csv(path, header=None))
+	df = np.sort(df, axis=0)
+	np.savetxt(dst+cloud, df)
+	print(path + " OK")
 
-for cloud in os.listdir(src):
-	if cloud.endswith("txt"):
-		path = src + cloud
-		df = np.array(pd.read_csv(path, header=None))
-		df = np.sort(df, axis=0)
-		np.savetxt(dst+cloud, df)
-		print(path + " OK")
+def batchanalyzes():
+	src = "/home/artur/pesquisa/centerprofile/nonneutral/"
+	dst = "/home/artur/pesquisa/centerprofile/nonneutral_sort/"
+
+	for cloud in os.listdir(src):
+		if cloud.endswith("txt"):
+			#path = src + cloud
+			#df = np.array(pd.read_csv(path, header=None))
+			#df = np.sort(df, axis=0)
+			#np.savetxt(dst+cloud, df)
+			#print(path + " OK")
+			analyzes(src, cloud, dst)
--- scripts/centerprofile.py
+++ scripts/centerprofile.py
@@ -1,13 +1,29 @@
 import os
 import subprocess
 
-datafolders = ["../nonneutral/"]
-outputdir = "../centerprofile/"
+# FUNCAO SHOW #01
+def centerprofile(folder, cloud, outputdir):
+	print("PROCESSANDO " + cloud)
+	filename = cloud.split(".")[0]
+	cloud = folder + cloud
+	subprocess.run(["../bin/cloudz", cloud, outputdir + filename + ".txt"])
 
-for folder in datafolders:
-	for cloud in os.listdir(folder):
-		filename = cloud.split(".")[0]
-		cloud = folder + cloud
-		subprocess.run(["../../bin/cloudz", cloud, outputdir + filename + ".txt"])
+# FUNCAO SHOW #02
+def batchprofile():
+	datafolders = ["../nonneutral/"]
+	outputdir = "../centerprofile/"
+
+	for folder in datafolders:
+		for cloud in os.listdir(folder):
+			#filename = cloud.split(".")[0]
+			#cloud = folder + cloud
+			#subprocess.run(["../../bin/cloudz", cloud, outputdir + filename + ".txt"])
+			centerprofile(folder, cloud, outputdir)
 		
-print("TERMINADO")
+	print("TERMINADO")
+
+centerprofile("../", "buddha.xyz", "../centerprofile/")
+centerprofile("../", "bunny.xyz", "../centerprofile/")
+centerprofile("../", "armadillo.xyz", "../centerprofile/")
+centerprofile("../", "lucy.xyz", "../centerprofile/")
+centerprofile("../", "head.xyz", "../centerprofile/")
