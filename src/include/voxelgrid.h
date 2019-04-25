08d523cd774390b73574138dfd19135d11efd3e7
Luiz Leite <luizleite@alu.ufc.br>
2019-04-24 21:08:47 -0300

    correcao de voxelgrid
    Correcao de erros numericos para subamostragens muito baixas (leafsize<0.4, por exemplo), melhoria no gerenciamento de memoria e na complexidade do algoritmo. OBS: devo tirar a funcao idx_offset de dentro de voxel.h e jogar para algum lugar que tenha mais sentido -- sera feito no proximo PR.

--- src/include/voxelgrid.h
+++ src/include/voxelgrid.h
@@ -11,10 +11,17 @@
 
 #include "cloud.h"
 
+uint idx_offset (uint i, uint j, uint k, uint size_x, uint size_y) {
+    return (k * size_x * size_y) + (j * size_x) + i;
+}
+
 struct cloud* cubic_voxel_grid(struct cloud* src, float leafsize) {
-    
+    if (leafsize <= 0.0) {
+        return NULL;
+    }
+
     float apothem = leafsize / 2;
-    
+
     struct vector3* centroid = cloud_get_center(src);
     
     struct vector3* axis_size = cloud_axis_size(src);
@@ -27,94 +34,117 @@ struct cloud* cubic_voxel_grid(struct cloud* src, float leafsize) {
     voxel_o->x = (cloud_min_x(src)->x + cloud_max_x(src)->x) / 2;
     voxel_o->y = (cloud_min_y(src)->y + cloud_max_y(src)->y) / 2;
     voxel_o->z = (cloud_min_z(src)->z + cloud_max_z(src)->z) / 2;
-    
+
     voxel_o->x -= (float) floor(apothem * num_voxels_x);
     voxel_o->y -= (float) floor(apothem * num_voxels_y);
     voxel_o->z -= (float) floor(apothem * num_voxels_z);
 
     uint pos_x, pos_y, pos_z;
     float d_x, min_x, max_x, d_y, min_y, max_y, d_z, min_z, max_z;
+    uint idx_aux;
 
     struct cloud* output = cloud_empty();
 
-    struct cloud* clouds[num_voxels_x][num_voxels_y][num_voxels_z];
-    for (uint i = 0; i < num_voxels_x; i++) {
-        for (uint j = 0; j < num_voxels_y; j++) {
-            for (uint k = 0; k < num_voxels_z; k++) {
-                clouds[i][j][k] = cloud_empty();
-            }
-        }
+    uint total_dim = num_voxels_x * num_voxels_y * num_voxels_z;
+
+    struct cloud** clouds = calloc (sizeof (struct cloud*), total_dim);
+    if (clouds == NULL) {
+        util_error("%s: erro alocando memoria cloud", __FUNCTION__);
+        return NULL;
     }
 
     for (uint n = 0; n < src->num_pts; n++) {
         d_x = src->points[n].x - voxel_o->x;
-        pos_x = (uint) ceil(d_x / leafsize);
-        min_x = voxel_o->x + (pos_x * leafsize) - apothem;
-        max_x = voxel_o->x + (pos_x * leafsize) + apothem;
-        if (src->points[n].x < centroid->x) {
-            if (src->points[n].x < min_x) {
-                pos_x--;
-            } else if (src->points[n].x >= max_x) {
-                pos_x++;
-            }
-        } else if (src->points[n].x > centroid->x) {
-            if (src->points[n].x <= min_x) {
-                pos_x--;
-            } else if (src->points[n].x > max_x) {
-                pos_x++;
+        if (d_x < 0.0) {
+            pos_x = 0;
+        } else {
+            pos_x = (uint) ceil(d_x / leafsize);
+        
+            min_x = voxel_o->x + (pos_x * leafsize) - apothem;
+            max_x = voxel_o->x + (pos_x * leafsize) + apothem;
+
+            if (src->points[n].x < centroid->x) {
+                if (src->points[n].x < min_x) {
+                    pos_x--;
+                } else if (src->points[n].x >= max_x) {
+                    pos_x++;
+                }
+            } else if (src->points[n].x > centroid->x) {
+                if (src->points[n].x <= min_x) {
+                    pos_x--;
+                } else if (src->points[n].x > max_x) {
+                    pos_x++;
+                }
             }
         }
-
+        
         d_y = src->points[n].y - voxel_o->y;
-        pos_y = (uint) ceil(d_y / leafsize);
-        min_y = voxel_o->y + (pos_y * leafsize) - apothem;
-        max_y = voxel_o->y + (pos_y * leafsize) + apothem;
-        if (src->points[n].y < centroid->y) {
-            if (src->points[n].y < min_y) {
-                pos_y--;
-            } else if (src->points[n].y >= max_y) {
-                pos_y++;
-            }
-        } else if (src->points[n].y > centroid->y) {
-            if (src->points[n].y <= min_y) {
-                pos_y--;
-            } else if (src->points[n].y > max_y) {
-                pos_y++;
+        if (d_y < 0.0) {
+            pos_y = 0;
+        } else {
+            pos_y = (uint) ceil(d_y / leafsize);
+            min_y = voxel_o->y + (pos_y * leafsize) - apothem;
+            max_y = voxel_o->y + (pos_y * leafsize) + apothem;
+            if (src->points[n].y < centroid->y) {
+                if (src->points[n].y < min_y) {
+                    pos_y--;
+                } else if (src->points[n].y >= max_y) {
+                    pos_y++;
+                }
+            } else if (src->points[n].y > centroid->y) {
+                if (src->points[n].y <= min_y) {
+                    pos_y--;
+                } else if (src->points[n].y > max_y) {
+                    pos_y++;
+                }
             }
         }
-
+        
         d_z = src->points[n].z - voxel_o->z;
-        pos_z = (uint) ceil(d_z / leafsize);
-        min_z = voxel_o->z + (pos_z * leafsize) - apothem;
-        max_z = voxel_o->z + (pos_z * leafsize) + apothem;
-        if (src->points[n].z < centroid->z) {
-            if (src->points[n].z < min_z) {
-                pos_z--;
-            } else if (src->points[n].z >= max_z) {
-                pos_z++;
-            }
-        } else if (src->points[n].z > centroid->z) {
-            if (src->points[n].z <= min_z) {
-                pos_z--;
-            } else if (src->points[n].z > max_z) {
-                pos_z++;
+        if (d_z < 0.0) {
+            pos_z = 0;
+        } else {
+            pos_z = (uint) ceil(d_z / leafsize);
+            min_z = voxel_o->z + (pos_z * leafsize) - apothem;
+            max_z = voxel_o->z + (pos_z * leafsize) + apothem;
+            if (src->points[n].z < centroid->z) {
+                if (src->points[n].z < min_z) {
+                    pos_z--;
+                } else if (src->points[n].z >= max_z) {
+                    pos_z++;
+                }
+            } else if (src->points[n].z > centroid->z) {
+                if (src->points[n].z <= min_z) {
+                    pos_z--;
+                } else if (src->points[n].z > max_z) {
+                    pos_z++;
+                }
             }
         }
 
-        cloud_add_point_vector(clouds[pos_x][pos_y][pos_z], &src->points[n]);
+        idx_aux = idx_offset(pos_x, pos_y, pos_z, num_voxels_x, num_voxels_y);
+
+        if (clouds[idx_aux] == 0) {
+            clouds[idx_aux] = cloud_empty();
+        }
+
+        cloud_add_point_vector(clouds[idx_aux], &src->points[n]);
     }
 
     for (uint i = 0; i < num_voxels_x; i++) {
         for (uint j = 0; j < num_voxels_y; j++) {
             for (uint k = 0; k < num_voxels_z; k++) {
-                if (clouds[i][j][k]->num_pts != 0) {
+                idx_aux = idx_offset(i, j, k, num_voxels_x, num_voxels_y);
+                if (clouds[idx_aux] != 0) {
                     cloud_add_point_vector(output, 
-                                           cloud_calc_center(clouds[i][j][k]));
+                                           cloud_calc_center(clouds[idx_aux]));
+                    cloud_free(clouds[idx_aux]);
                 }
-                cloud_free(clouds[i][j][k]);
             }
         }
     }
+    free(clouds);
+
     return output;
 }
 
