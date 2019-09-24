a5bd16f1b5b8cbef6ce52cf5128ef790da961f5f
Artur Rodrigues Rocha Neto <artur.rodrigues26@gmail.com>
2019-09-24 19:15:27 -0300

    eita que hoje vai ser louko

--- pontu/benchmarks/testing.c
+++ pontu/benchmarks/testing.c
@@ -3,13 +3,50 @@
 #include "../pontu_features.h"
 #include "../pontu_sampling.h"
 
+int has_point(struct vector3 **points, uint n, struct vector3 *p)
+{
+	if (p == NULL)
+		return 0;
+	
+	for (uint i = 0; i < n; i++)
+		if (p == points[i])
+			return 1;
+	
+	return 0;
+}
+
+struct cloud *neighbors(struct cloud *source, struct cloud *target)
+{
+	struct cloud *ans = cloud_empty();
+	struct vector3 *srcpt = NULL;
+	struct vector3 *tgtpt = NULL;
+	struct vector3 **points = malloc(target->numpts * sizeof(struct vector3 *));
+	int n = 0;
+	
+	real d = 0.0f;
+	for (uint i = 0; i < source->numpts; i++) {
+		d = cloud_nearest_neighbors_bruteforce(source, target, &srcpt, &tgtpt);
+		vector3_debug(srcpt, stdout);
+		vector3_debug(tgtpt, stdout);
+		printf("d = %f\n", d);		
+	}
+	
+	free(points);
+	
+	return ans;
+}
+
 int main()
 {
-	struct cloud *src = cloud_load_xyz("../samples/bunny.xyz");
-	struct cloud *tgt = cloud_load_xyz("../samples/bunny_trans.xyz");
+	struct cloud *source = cloud_load_ply("../samples/bun045.ply");
+	struct cloud *target = cloud_load_ply("../samples/bun000.ply");
 	
-	printf("||src|| = %u\n||tgt|| = %u\n", src->numpts, tgt->numpts);
+	struct cloud *ans = neighbors(source, target);
+	cloud_free(ans);
 	
+	
+	
+	/**
 	struct vector3 *src_bf;
 	struct vector3 *tgt_bf;
 	clock_t time_bf = clock();
@@ -31,9 +68,11 @@ int main()
 	vector3_debug(src_kd, stdout);
 	vector3_debug(tgt_kd, stdout);
 	printf("d_kd = %f | time_kd = %ld\n", d_kd, time_kd);
+	*/
 	
-	cloud_free(tgt);
-	cloud_free(src);
+	
+	cloud_free(target);
+	cloud_free(source);
 	
 	return 0;
 }
